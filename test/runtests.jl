e179d6a25126461fb3a22707fbd264a495cf4461
Benoit Pasquier <briochemc@gmail.com>
2020-06-29 11:51:39 +1000

    Fix get_dataset + add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,13 @@ using CMAP
 using Test
 using DataFrames
 
-@testset "CMAP.jl" begin    
+@testset "CMAP.jl" begin
     status, response = query(API(), "select * from tblMakes")
     @test nrow(response) == 3
+end
+
+@testset "Gradients 2" begin
+    @testset "Nutrients" begin
+        @test get_dataset("tblMGL1704_Gradients2_Nutrients") isa DataFrame
+    end
 end
